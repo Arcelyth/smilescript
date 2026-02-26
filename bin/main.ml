bc501575ca9e1cb7b427d5fef6e143a9c668ab10
Arcelyth <arcelyth@tutamail.com>
2026-02-27 02:00:15 +0800

    add examples; REPL support expression

--- bin/main.ml
+++ bin/main.ml
@@ -18,16 +18,30 @@ let read_file path =
     eprintf "Error: %s\n" err;
     exit 64 
 
-
-let run line state = 
+let run line state is_repl =
   let scanner = { Lexer.source=line; start=0; current=0; line_num=1} in
   let tokens = Lexer.scan_tokens scanner state in
-  let stmts = Parser.parse tokens state in
-  Interpreter.interpret stmts state
+
+  if is_repl then
+    try
+      let expr, rest = Parser.expression tokens in
+      match rest with
+      | [{kind=Lexer.EOF; _}] | [] ->
+          let v = Interpreter.evaluate_expr expr state in
+          print_endline (Interpreter.string_of_value v)
+      | _ -> raise Exit
+    with _ ->
+      try
+        let stmts = Parser.parse tokens state in
+        Interpreter.interpret stmts state 
+      with e -> eprintf "Error: %s\n" (Printexc.to_string e)
+  else
+    let stmts = Parser.parse tokens state in
+    Interpreter.interpret stmts state
 
 let run_file path state = 
   let content = read_file path in
-  run content state; 
+  run content state false; 
   if state.had_err = true then exit(65);
   if state.had_runtime_err = true then exit(70)
 
@@ -37,7 +51,7 @@ let rec run_prompt state =
   match (try Some (input_line stdin) with End_of_file -> None) with
   | None -> print_newline (); exit 0 
   | Some line ->
-      (try run line state with e -> eprintf "Error: %s\n" (Printexc.to_string e));
+      (try run line state true with e -> eprintf "Error: %s\n" (Printexc.to_string e));
       state.had_err <- false;
       run_prompt state
 
